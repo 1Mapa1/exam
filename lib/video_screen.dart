63db4ae93d820c93f59444ac6715c7075a2ad81e
Pavel Maslov <maslovpavel333@gmail.com>
2023-11-26 23:43:55 +0300

    +

--- lib/video_screen.dart
+++ lib/video_screen.dart
@@ -8,9 +8,8 @@ class VideoScreen extends StatefulWidget {
 
 class _VideoScreenState extends State<VideoScreen> {
   late VideoPlayerController _controller = VideoPlayerController.networkUrl(
-        Uri.parse('https://cdn.discordapp.com/attachments/761299040851460108/1178424184074940608/Rick_Astley_-_Never_Gonna_Give_You_Up_Official_Music_Video.mp4'))
+        Uri.parse('https://cdn.discordapp.com/attachments/761299040851460108/1178430651058368512/1.mp4'))
       ..initialize().then((_) {
-        _controller.play();
         setState(() {});
       });
 
@@ -21,12 +20,50 @@ class _VideoScreenState extends State<VideoScreen> {
         title: Text('Воспроизведение видео из интернета'),
       ),
       body: Center(
-        child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : CircularProgressIndicator(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                : CircularProgressIndicator(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0), // Отступ слева и справа
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _controller.play();
+                    },
+                    child: Text('Старт'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0), // Отступ слева и справа
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _controller.pause();
+                    },
+                    child: Text('Пауза'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0), // Отступ слева и справа
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _controller.seekTo(Duration.zero);
+                      _controller.pause();
+                    },
+                    child: Text('Стоп'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
