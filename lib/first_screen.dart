44eaafa2baab8cdd2976e4ac878b0cd6562606d9
Pavel Maslov <maslovpavel333@gmail.com>
2023-11-26 20:51:49 +0300

    Фиксация

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,30 +1,98 @@
-import 'package:exam/second_screen.dart';
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
-        title: const Text("Экран 1"),
+        title: Text('Простой Калькулятор'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blueAccent)
-              ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
-              },
-              child: const Text('Нажми на меня'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Ответ", 
+                style: TextStyle(fontSize: 48.0)
+              )
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                buildButton("1"),
+                buildButton("2"),
+                buildButton("3"),
+                buildButton("/")
+              ]
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                buildButton("4"),
+                buildButton("5"),
+                buildButton("6"),
+                buildButton("*")
+              ]
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                buildButton("7"),
+                buildButton("8"),
+                buildButton("9"),
+                buildButton("-")
+              ]
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                buildButton("."),
+                buildButton("0"),
+                buildButton("C"),
+                buildButton("+")
+              ]
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                buildButton("=")
+              ] 
+            ),
+          )
+        ]
       )
+    );
+
+    
+  }
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      
+      child: Container(
+        height: double.maxFinite,
+        margin: EdgeInsets.all(10),
+        child: ElevatedButton(
+          child: Text(
+            buttonText,
+            style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => (),
+        ),
+      ),
     );
   }
 }
