19be3ef179913a0671a5e3389fa4a89447cd2dc9
Pavel Maslov <maslovpavel333@gmail.com>
2023-11-27 02:43:34 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -19,71 +19,63 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ClothingItemScreen(),
+      home: StationeryProductDetailsScreen(),
     );
   }
 }
 
-class ClothingItem {
-  final String name;
-  final String category;
-  final double price;
-  final String imageUrl;
-
-  ClothingItem({
-    required this.name,
-    required this.category,
-    required this.price,
-    required this.imageUrl,
-  });
-}
-
-class ClothingItemScreen extends StatelessWidget {
-  final ClothingItem clothingItem = ClothingItem(
-    name: 'Джинсовая куртка',
-    category: 'Верхняя одежда',
-    price: 79.99,
-    imageUrl: 'assets/denim_jacket.png',
-  );
-
+class StationeryProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Магазин одежды'),
+        title: Text('Подробности о товаре'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
-              child: Image.asset(
-                clothingItem.imageUrl,
-                width: 200.0,
-                height: 200.0,
-                fit: BoxFit.cover,
-              ),
+            // Изображение товара
+            Image.asset(
+              'assets/glam.png',
+              height: 200,
+              width: double.infinity,
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 16),
+            // Информация о товаре
             Text(
-              clothingItem.name,
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              'Лак для ногтей "Glam Nails"',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8.0),
+            SizedBox(height: 8),
+            // Описание товара
             Text(
-              clothingItem.category,
-              style: TextStyle(color: Colors.grey),
+              'Изысканный лак для ногтей, созданный для подчеркивания '
+                  'красоты ваших ногтей. Широкий выбор цветов и стойкость.',
+              style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 8.0),
+            SizedBox(height: 16),
+            // Информация о цене и наличии
             Text(
-              '\$${clothingItem.price.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 18.0, color: Colors.blue),
+              'Цена: \$8.99',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16.0),
+            Text(
+              'В наличии: 30 штук',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 16),
+            // Кнопка для добавления в корзину
             ElevatedButton(
               onPressed: () {
-                // Добавьте обработчик для добавления товара в корзину или детали заказа
+                // Добавить товар в корзину
+                // Реализуйте соответствующую логику
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Товар добавлен в корзину'),
+                  ),
+                );
               },
               child: Text('Добавить в корзину'),
             ),
