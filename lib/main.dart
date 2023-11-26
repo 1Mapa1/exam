518901c9ad9bb389299b53d51ac765290a575fe7
Pavel Maslov <maslovpavel333@gmail.com>
2023-11-27 02:29:14 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:exam/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +7,89 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Exam_1',
-      home: LoginScreen(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ClothingItemScreen(),
+    );
+  }
+}
+
+class ClothingItem {
+  final String name;
+  final String category;
+  final double price;
+  final String imageUrl;
+
+  ClothingItem({
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.imageUrl,
+  });
+}
+
+class ClothingItemScreen extends StatelessWidget {
+  final ClothingItem clothingItem = ClothingItem(
+    name: 'Джинсовая куртка',
+    category: 'Верхняя одежда',
+    price: 79.99,
+    imageUrl: 'assets/denim_jacket.png',
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Магазин одежды'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.asset(
+                clothingItem.imageUrl,
+                width: 200.0,
+                height: 200.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              clothingItem.name,
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              clothingItem.category,
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              '\$${clothingItem.price.toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 18.0, color: Colors.blue),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Добавьте обработчик для добавления товара в корзину или детали заказа
+              },
+              child: Text('Добавить в корзину'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
